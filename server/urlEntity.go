7103f92f5650b3c5946e7a9c76f5e475a0dfda6a
unknown <emanuelisc@gmail.com>
2018-11-12 00:03:00 +0200

    UrlEntity server fix

--- server/urlEntity.go
+++ server/urlEntity.go
@@ -18,7 +18,7 @@ type Url struct {
 	Name        string        `json:"name" bson:"name"`
 	Ip          string        `json:"ip" bson:"ip"`
 	Description string        `json:"description" bson:"description"`
-	Servers     string        `json:"servers" bson:"servers"`
+	Servers     []Server        `json:"servers" bson:"servers"`
 	User        string		 		`json:"user" bson:"user"`
 	CreatedAt   time.Time     `json:"createdAt" bson:"created_at"`
 }
